e2ad3c6911715d745a3001bc6df1b9eeb562f672
Anvay <anvay.varerkar@gmail.com>
2019-01-07 00:59:27 +0530

    Added Helpline Screen

--- screens/HelplineNavigator.js
+++ screens/HelplineNavigator.js
@@ -22,9 +22,48 @@ export default class HelplineScreen extends Component {
     }
     render() {
         return (
-            <View style={styles.container}>
-                <Text>HelplineScreen </Text>
-                <Text>the clan is {this.props.screenProps.clan}</Text>
+
+            <View style={{flex: 1, flexDirection:'column'}}>
+       <View style={{height:50, flexDirection:'row', justifyContent:'center', marginTop:5,  backgroundColor: '#639fff',}}><Text style={{fontSize: 30,}}> Hospital</Text></View>
+            <View style={{flex: 0.5,  flexDirection:'row', justifyContent: "space-around"}}>
+                
+               <View style={{ flex:0.5, justifyContent: 'flex-start',paddingLeft:8}}>
+                <Text style={{fontSize: 20, marginVertical:7}}>Ambulance</Text> 
+                <Text style={{fontSize: 20, marginVertical:7}}>Visiting Doctors</Text>
+                <Text style={{fontSize: 20, marginVertical:7}}> IITB Hospital</Text>
+                <Text style={{fontSize: 20, marginVertical:7}}>
+        
+                </Text>
+                   <Text style={{fontSize: 20}}></Text>       
+            </View>
+            <View style={{ flex:0.5, justifyContent: 'flex-start'}}>
+            <Text style={{fontSize: 20, marginVertical:7,}}>1101</Text>
+            <Text style={{fontSize: 20, marginVertical:7,}}>7053</Text>
+            <Text style={{fontSize: 20, marginVertical:7,}}>+91-2225722149</Text>
+            <Text style={{fontSize: 20, marginVertical:7,}}>1110</Text>
+            <Text style={{fontSize: 20, marginVertical:7,}}>7051</Text>
+            </View>
+            </View>
+
+            <View style={{height:50, flexDirection:'row', justifyContent:'center', backgroundColor: '#639fff',}}><Text style={{fontSize: 30}}> Security Section</Text></View>
+            <View style={{flex: 0.5,  flexDirection:'row', justifyContent: "space-around"}}>
+                
+               <View style={{ flex:0.5, justifyContent: 'flex-start',paddingLeft:8}}>
+                <Text style={{fontSize: 20, marginVertical:7}}>Main Gate</Text> 
+                <Text style={{fontSize: 20, marginVertical:7}}>  </Text>       
+                <Text style={{fontSize: 20, marginVertical:7}}>Office</Text>
+                <Text style={{fontSize: 20, marginVertical:7}}>Police Station Powai</Text>
+                <Text style={{fontSize: 20, marginVertical:7}}>Fire Control Room</Text>
+                   
+            </View>
+            <View style={{ flex:0.5, justifyContent: 'flex-start'}}>
+            <Text style={{fontSize: 20, marginVertical:7}}>7051</Text>
+            <Text style={{fontSize: 20, marginVertical:7}}>1110</Text>
+            <Text style={{fontSize: 20, marginVertical:7}}>8989</Text>
+            <Text style={{fontSize: 20, marginVertical:7}}>022 25702690</Text>
+            <Text style={{fontSize: 20, marginVertical:7}}>022 23076111</Text>
+            </View>
+            </View>
             </View>
         );
     }
